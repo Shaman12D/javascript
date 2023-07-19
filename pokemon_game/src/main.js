ed3bbf2765401ee924249732ad4a0f6b49d31b67
Shaman12D <parkista258@gmail.com>
2023-07-19 18:30:21 -0500

    pokemon

---
+++ pokemon_game/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@/css/animation.css'
+import '@/css/style.css'
+
+createApp(App).mount('#app')
---
+++ pokemon_game/src/pages/PokemonPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>¿Quién es ese pokémon?</h1>
+    <PokemonPicture :pokemonId="4" :showPokemon="true"/>
+    <PokemonOptions :pokemons="pokemonArr"/>
+</template>
+
+<script>
+    import PokemonPicture from '@/components/PokemonPicture'
+    import PokemonOptions from '@/components/PokemonOptions'
+
+    import getPokemonOption from '@/helpers/getPokemonOptions'
+
+    // console.log(getPokemonOption())
+
+    export default {
+        components: {PokemonPicture, PokemonOptions},
+        data(){
+            return{
+                pokemonArr: []
+            }
+        },
+        methods: {
+            async mixPokemonArray(){
+                this.pokemonArr = await getPokemonOption()
+            }
+        },
+        mounted(){
+            this.mixPokemonArray()
+        }
+    }
+</script>
+
+<style>
+
+</style>
